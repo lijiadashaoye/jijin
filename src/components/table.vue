c479d677f3b82befd51b9309535a164055e722e2
lipeng <15646507519@sina.cn>
2022-11-22 12:18:47 +0800

    oiuyfg

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,32 +1,5 @@
 <template >
   <div v-if="isOk" class="tableWap">
-    <h3 style="text-indent: 45%">持仓情况</h3>
-    <table class="shouyi">
-      <tr>
-        <th>基金名称</th>
-        <th>持仓数额</th>
-        <th>盈亏数额</th>
-        <th>收益率</th>
-      </tr>
-      <tr v-for="(t, i) of shouyiArr" :key="i">
-        <td>
-          <p @contextmenu.prevent="seeJiJin(t.code, $event)">
-            {{ t.name }}（ {{ t.code }}）
-            <span @click="seejijin(showJiJin)" v-if="showJiJin === t.code"
-              :style="{ top: spanTop, right: spanLeft }">查看基金</span>
-          </p>
-        </td>
-        <td>
-          {{ t.chicang }}
-        </td>
-        <td>
-          <span :class="{isGreen:!setColor(t.yingli),isRed:setColor(t.yingli)}">{{ t.yingli}}</span>
-        </td>
-        <td>
-          <span :class="{isGreen:!setColor(t.yingli),isRed:setColor(t.yingli)}">{{ t.shouyilv }}</span>
-        </td>
-      </tr>
-    </table>
     <h3 style="text-indent: 45%">重合的股票分析</h3>
     <table class="chonghe">
       <tr>
@@ -38,13 +11,21 @@
         <td>
           <p @contextmenu.prevent="seeJiJin(t.one.code, $event)">
             {{ t.one.name }}
-            <span @click="seejijin(showJiJin)" v-if="showJiJin === t.one.code"
-              :style="{ top: spanTop, right: spanLeft }">查看基金</span>
+            <span
+              @click="seejijin(showJiJin)"
+              v-if="showJiJin === t.one.code"
+              :style="{ top: spanTop, right: spanLeft }"
+              >查看基金</span
+            >
           </p>
           <p @contextmenu.prevent="seeJiJin(t.two.code, $event)">
             {{ t.two.name }}
-            <span @click="seejijin(showJiJin)" v-if="showJiJin === t.two.code"
-              :style="{ top: spanTop, right: spanLeft }">查看基金</span>
+            <span
+              @click="seejijin(showJiJin)"
+              v-if="showJiJin === t.two.code"
+              :style="{ top: spanTop, right: spanLeft }"
+              >查看基金</span
+            >
           </p>
         </td>
         <td>
@@ -65,15 +46,59 @@
         <th>持有基金</th>
       </tr>
       <tr v-for="t of gupiaoEnd" :key="t.code">
-        <td :style="{
-          background: t.jijin.length > 4 ? 'pink' : '',
-        }" @contextmenu.prevent="showRight(t.code, $event)">
+        <td
+          :style="{
+            background: t.jijin.length > 4 ? 'pink' : '',
+          }"
+          @contextmenu.prevent="showRight(t.code, $event)"
+        >
           {{ t.name }}
-          <span @click="seeGuPiao(showright)" v-if="showright === t.code"
-            :style="{ top: spanTop, right: spanLeft }">查看股票</span>
+          <span
+            @click="seeGuPiao(showright)"
+            v-if="showright === t.code"
+            :style="{ top: spanTop, right: spanLeft }"
+            >查看股票</span
+          >
         </td>
         <td>{{ t.jijin.length }}</td>
         <td>{{ t.jijin.join("、 ") }}</td>
+      </tr>
+    </table>
+    <h3 style="text-indent: 45%">持仓情况</h3>
+    <table class="shouyi">
+      <tr>
+        <th>基金名称</th>
+        <th>持仓数额</th>
+        <th>盈亏数额</th>
+        <th>收益率</th>
+      </tr>
+      <tr v-for="(t, i) of shouyiArr" :key="i" :class="{ toSell: t.sell }">
+        <td>
+          <p @contextmenu.prevent="seeJiJin(t.code, $event)">
+            {{ t.name }}（ {{ t.code }}）
+            <span
+              @click="seejijin(showJiJin)"
+              v-if="showJiJin === t.code"
+              :style="{ top: spanTop, right: spanLeft }"
+              >查看基金</span
+            >
+          </p>
+        </td>
+        <td>
+          {{ t.chicang }}
+        </td>
+        <td>
+          <span
+            :class="{ isGreen: !setColor(t.yingli), isRed: setColor(t.yingli) }"
+            >{{ t.yingli }}</span
+          >
+        </td>
+        <td>
+          <span
+            :class="{ isGreen: !setColor(t.yingli), isRed: setColor(t.yingli) }"
+            >{{ t.shouyilv }}</span
+          >
+        </td>
       </tr>
     </table>
   </div>
@@ -93,19 +118,21 @@ let isOk = ref(false),
   showJiJin = ref(""),
   spanTop = ref(0),
   spanLeft = ref(0),
-  time = ref(''),
-  shouyiArr = ref([]);
+  time = ref(""),
+  shouyiArr = ref([]),
+  metaData = ref([]); // 从excel文件中读取到的基金
 (function autoRead() {
   $axios({
     method: "get",
     url: `wenjian`,
   }).then((res) => {
     let data = res.data.data
-      .slice(1)
-      .map((t) => ["" + t[0], t[1]])
-      .filter((t) => t[0] !== "undefined"),
+        .slice(1)
+        .map((t) => ["" + t[0], t[1]])
+        .filter((t) => t[0] !== "undefined"),
       zhengli = [];
-    for (let i = data.length; i--;) {
+    metaData.value = data;
+    for (let i = data.length; i--; ) {
       let t = data[i];
       if (!zhengli.includes(t[0])) {
         zhengli.push(t[0]);
@@ -127,38 +154,37 @@ let isOk = ref(false),
         getData();
       });
     } else {
-      getData();  // 使用本地存储的数据
+      getData(); // 使用本地存储的数据
     }
   });
 })();
 // 使用本地存储的数据
 function getData() {
-    $axios({
-      method: "get",
-      url: `getPageData`,
-      headers: {
-        "Content-Type": "tapplication/json;charset=utf-8",
-      },
-    })
-    .then(async (res) => {
-      let data = res.data;
-      if (data.length) {
-        let now = shaiguo.value.map((t) => t[0]);
-        data.forEach((t) => {
-          // 修改名字，与excel内一致了
-          let tar = shaiguo.value.filter((j) => j[0] === t[0]);
-          if (tar.length) {
-            t[1] = tar[0][1];
-          }
-          if (!now.includes(t[0])) {
-            data = data.filter((s) => s[0] !== t[0]);
-          } else {
-            shaiguo.value = shaiguo.value.filter((s) => s[0] !== t[0]);
-          }
-        });
-      }
-      second(data);
-    });
+  $axios({
+    method: "get",
+    url: `getPageData`,
+    headers: {
+      "Content-Type": "tapplication/json;charset=utf-8",
+    },
+  }).then(async (res) => {
+    let data = res.data;
+    if (data.length) {
+      let now = shaiguo.value.map((t) => t[0]);
+      data.forEach((t) => {
+        // 修改名字，与excel内一致了
+        let tar = shaiguo.value.filter((j) => j[0] === t[0]);
+        if (tar.length) {
+          t[1] = tar[0][1];
+        }
+        if (!now.includes(t[0])) {
+          data = data.filter((s) => s[0] !== t[0]);
+        } else {
+          shaiguo.value = shaiguo.value.filter((s) => s[0] !== t[0]);
+        }
+      });
+    }
+    second(data);
+  });
 }
 async function second(data) {
   let postData = [];
@@ -198,20 +224,20 @@ async function second(data) {
 function zhengliFn(data) {
   let jijinData = [];
   let gupiao = data.reduce((all, now) => {
-    jijinData.push({
-      code: now[0],
-      name: now[1],
-      gupiao: now[2].map((s) => s.name),
-    });
-    let tar = now[2];
-    tar.forEach((t) => {
-      if (!t.jijin) {
-        t.jijin = [now[1]];
-      }
-      all.push(t);
-    });
-    return all;
-  }, []),
+      jijinData.push({
+        code: now[0],
+        name: now[1],
+        gupiao: now[2].map((s) => s.name),
+      });
+      let tar = now[2];
+      tar.forEach((t) => {
+        if (!t.jijin) {
+          t.jijin = [now[1]];
+        }
+        all.push(t);
+      });
+      return all;
+    }, []),
     kong = [];
   gupiaoEnd.value = gupiao
     .reduce((all, now) => {
@@ -231,7 +257,7 @@ function zhengliFn(data) {
     gupiao = jijinData[i].gupiao;
     for (let j = 0; j < arr.length; j++) {
       let gupiao2 = arr[j].gupiao;
-      for (let k = gupiao2.length; k--;) {
+      for (let k = gupiao2.length; k--; ) {
         if (gupiao.includes(gupiao2[k])) {
           num++;
         }
@@ -288,57 +314,67 @@ function seejijin(code) {
 }
 function setColor(str) {
   let reg = /^-/;
-  return !reg.test(str)
+  return !reg.test(str);
 }
 // 使用从天天基金网获取的收益数据
 function useHttps() {
-  import('../../myself.js').then(res=>{
-  let reg = /<tr>.+?<\/tr>/ig,
-    arr = JSON.parse(res.default).content.match(reg),
-    reg1 = /(?<=html'>).+?(?:\（)\d{6}(?:\）)/ig, // 基金名字和代码
-    reg2 = /(.+)(?:\（)(\d{6})(?:\）)/ig, // 基金代码
-    reg3 = /(?<=desc'>).+?(?=(<\/td|<br\/))/ig,  // 持仓
-    reg4 = /(?<=f16'>)-?(\d+?,?)+?(\.\d+)(?=<\/span)/ig,
-    reg5 = /(?<=f12'>)-?\d+?(\.)?\d+?%/ig; // 收益率
-  shouyiArr.value = arr.map(t => {
-    let is1 = t.match(reg3),
-      is2 = t.match(reg4),
-      is3 = t.match(reg5),
-      obj = {
-        chicang: is1 ? is1[0] : '',
-        yingli: is2 ? is2[0] : '',
-        shouyilv: is3 ? is3[0] : '',
-      },
-      num = obj.shouyilv.replace('%', '');
-    if (!isNaN(+num)) {
-      obj.num = +num
-    }
-    t.match(reg1)[0].replace(reg2, ($0, $1, $2) => {
-      obj.name = $1;
-      obj.code = $2;
-    })
-    return obj
-  })
-    .sort((a, b) => a.num - b.num)
-})
+  import("../../myself.js").then((res) => {
+    let reg = /<tr>.+?<\/tr>/gi,
+      arr = JSON.parse(res.default).content.match(reg),
+      reg1 = /(?<=html'>).+?(?:\（)\d{6}(?:\）)/gi, // 基金名字和代码
+      reg2 = /(.+)(?:\（)(\d{6})(?:\）)/gi, // 基金代码
+      reg3 = /(?<=desc'>).+?(?=(<\/td|<br\/))/gi, // 持仓
+      reg4 = /(?<=f16'>)-?(\d+?,?)+?(\.\d+)(?=<\/span)/gi,
+      reg5 = /(?<=f12'>)-?\d+?(\.)?\d+?%/gi; // 收益率
+    shouyiArr.value = arr
+      .map((t) => {
+        let is1 = t.match(reg3),
+          is2 = t.match(reg4),
+          is3 = t.match(reg5),
+          obj = {
+            chicang: is1 ? is1[0] : "",
+            yingli: is2 ? is2[0] : "",
+            shouyilv: is3 ? is3[0] : "",
+          },
+          num = obj.shouyilv.replace("%", "");
+        if (!isNaN(+num)) {
+          obj.num = +num;
+        }
+        t.match(reg1)[0].replace(reg2, ($0, $1, $2) => {
+          obj.name = $1;
+          obj.code = $2;
+        });
+        return obj;
+      })
+      .sort((a, b) => a.num - b.num);
+
+    let excelCodeArr = metaData.value.map((s) => s[0]);
+    shouyiArr.value.forEach((t) => {
+      if (!excelCodeArr.includes(t.code)) {
+        t.sell = true;
+      }
+    });
+  });
 }
 // 读取本地收益文件
 function getShouYi() {
   $axios({
-      method: "get",
-      url: `shouyi`,
-      headers: {
-        "Content-Type": "tapplication/json;charset=utf-8",
-      },
-    }).then(res=>{
-      shouyiArr.value = res.data.map(t => {
-    let num = t.shouyilv.replace('%', '');
-    if (!isNaN(+num)) {
-      t.num = +num
-    }
-    return t
-  }).sort((a, b) => a.num - b.num);
-    })
+    method: "get",
+    url: `shouyi`,
+    headers: {
+      "Content-Type": "tapplication/json;charset=utf-8",
+    },
+  }).then((res) => {
+    shouyiArr.value = res.data
+      .map((t) => {
+        let num = t.shouyilv.replace("%", "");
+        if (!isNaN(+num)) {
+          t.num = +num;
+        }
+        return t;
+      })
+      .sort((a, b) => a.num - b.num);
+  });
 }
 </script>
 <style scoped lang='scss'>
@@ -530,7 +566,7 @@ p {
   tr {
     td {
       font-size: 14px;
-      text-align: center
+      text-align: center;
     }
 
     td:nth-of-type(1) {
@@ -561,10 +597,13 @@ p {
 }
 
 .isGreen {
-  color: green
+  color: green;
 }
 
 .isRed {
-  color: rgb(242, 50, 50)
+  color: rgb(242, 50, 50);
+}
+.toSell {
+  background-color: rgb(155, 242, 252);
 }
 </style>
